bd142db2f7b5bc5c7640a8bd39163acd5fe6bdfe
Joe Abrams <jabrams@pandora.com>
2019-03-04 20:00:43 -0800

    use Headers type for default

--- packages/graphql-playground-react/src/types.ts
+++ packages/graphql-playground-react/src/types.ts
@@ -32,5 +32,5 @@ export interface ISettings {
   ['schema.polling.endpointFilter']: string
   ['schema.polling.interval']: number
   ['tracing.hideTracingResponse']: boolean
-  ['request.globalHeaders']: {}
+  ['request.globalHeaders']: Headers
 }
